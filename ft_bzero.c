65d6a70a84a05ee02099c46de43db57e28692c8c
Muhammet Ali Disbudak <mdisbuda@k2m11s03.42kocaeli.com.tr>
2026-01-14 12:44:09 +0300

    create tghe

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mdisbuda <mdisbuda@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 13:29:52 by mdisbuda          #+#    #+#             */
-/*   Updated: 2026/01/07 14:58:47 by mdisbuda         ###   ########.fr       */
+/*   Updated: 2026/01/14 12:40:59 by mdisbuda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t i;
-	unsigned char* mem; 
-	
-	mem = (unsigned char*)s;
-	i = 0;
-	while(i < n)
-	{
-		mem[i] = (unsigned char)0;
-		i++;
-	}
+	ft_memset(&s,0,n);
 }
